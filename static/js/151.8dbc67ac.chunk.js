9ad17f0f6d09d9853458cf2ce4f0b0f546e070f6
Jan Balangue <jan.a.balangue@gmail.com>
2024-07-09 16:47:45 -0700

    Updates

---
+++ static/js/151.8dbc67ac.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrouter_app=self.webpackChunkrouter_app||[]).push([[151],{151:(e,t,s)=>{s.r(t),s.d(t,{default:()=>l});var a=s(579);function l(){return(0,a.jsxs)("div",{className:"text-center p-5 text-xl",children:[(0,a.jsx)("h1",{className:"text-xl text-slate-800",children:"Admin Page"}),(0,a.jsx)("p",{className:"text-base text-slate-800",children:"You shouldn't come here often cause I'm lazy."})]})}}}]);
+//# sourceMappingURL=151.8dbc67ac.chunk.js.map
